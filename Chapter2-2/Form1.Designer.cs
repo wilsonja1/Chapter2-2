fd1496135eaf24a42a85a1b046357ec4780aa3ec
Jack Wilson <battlejackmaster@gmail.com>
2018-06-29 14:58:24 -0700

    color changer. also added image.

--- Chapter2-2/Form1.Designer.cs
+++ Chapter2-2/Form1.Designer.cs
@@ -28,9 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.enableCheckBox = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -41,16 +45,19 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Change the label if checked";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // checkBox1
+            // enableCheckBox
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(339, 13);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(136, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Enable Label Changing";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.enableCheckBox.AutoSize = true;
+            this.enableCheckBox.Checked = true;
+            this.enableCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckBox.Location = new System.Drawing.Point(339, 13);
+            this.enableCheckBox.Name = "enableCheckBox";
+            this.enableCheckBox.Size = new System.Drawing.Size(136, 17);
+            this.enableCheckBox.TabIndex = 1;
+            this.enableCheckBox.Text = "Enable Label Changing";
+            this.enableCheckBox.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -61,16 +68,39 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Press the button to change my text";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 58);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 91);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.enableCheckBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "My Desktop App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,8 +109,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox enableCheckBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Chapter2-2/Form1.cs
+++ Chapter2-2/Form1.cs
@@ -16,5 +16,37 @@ namespace Chapter2_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(enableCheckBox.Checked == true)
+            {
+                if(label1.Text == "Right")
+                {
+                    label1.Text = "Left";
+                    label1.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    label1.Text = "Right";
+                    label1.TextAlign = ContentAlignment.MiddleRight;
+                }
+            }
+            else
+            {
+                label1.Text = "Text changing is disabled";
+                label1.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int c = 254; c>=0; c--)
+            {
+                this.BackColor = Color.FromArgb(c, 255 - c, c);
+                Application.DoEvents();
+            }
+
+        }
     }
 }
